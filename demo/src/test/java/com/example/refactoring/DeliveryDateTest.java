52f6e6d6194513c3998144f625ac113e16d9efea
知念英弥 <s19013@std.it-college.ac.jp>
2024-01-24 22:41:42 +0900

    テスト作成

--- demo/src/test/java/com/example/refactoring/DeliveryDateTest.java
+++ demo/src/test/java/com/example/refactoring/DeliveryDateTest.java
@@ -6,27 +6,30 @@ import org.junit.jupiter.api.Test;
 import com.example.refactoring.DeliveryDate;
 
 class DeliveryDateTest {
-	@Test
-	void 配送日のテスト() {
-		/*new DeliveryDate();
-		assertEquals(1,1);
-		new DeliveryDate().getDeliveryDate();
-		assertEquals(1,1);*/
-		LocalDate actualDate = new DeliveryDate().getDeliveryDate();
-		assertEquals(LocalDate.of(2024,1,31),actualDate);
-	}
 
 	@Test
 	//void _配送日のテスト() {
 	void _小の月の月末になる場合のテスト() {
-		LocalDate actualDate = new DeliveryDate().getDeliveryDate();
+		FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+		deliveryDate.setDate(LocalDate.of(2020,9,13));
+		LocalDate actualDate = deliveryDate.getDeliveryDate();
 		assertEquals(LocalDate.of(2020,9,30),actualDate);
 	}
 
 	@Test
 	void _大の月の月末になる場合のテスト() {
-		LocalDate actualDate = new DeliveryDate().getDeliveryDate();
+		FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+		deliveryDate.setDate(LocalDate.of(2020,10,13));
+		LocalDate actualDate = deliveryDate.getDeliveryDate();
 		assertEquals(LocalDate.of(2020,10,31),actualDate);
 	}
 
+}
+
+
+class FakeDeliveryDate extends DeliveryDate {
+	private LocalDate date;
+	@Override
+	protected LocalDate getNow() {return date;}
+	public void setDate(LocalDate date) { this.date = date; }
 }
