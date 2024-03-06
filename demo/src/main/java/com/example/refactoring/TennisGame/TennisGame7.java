65ef11efa37f1f3fa7e501480a3a096f7f3e9882
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-06 22:11:58 +0900

    スコア計算を実装

--- demo/src/main/java/com/example/refactoring/TennisGame/TennisGame7.java
+++ demo/src/main/java/com/example/refactoring/TennisGame/TennisGame7.java
@@ -5,14 +5,8 @@ public class TennisGame7 implements TennisGame{
     private final String player2Name;
 
     //獲得したポイント数
-    private int player1point;
-    private int player2point;
-
-    //獲得したゲーム数
-    private int player1Games;
-    private int player2Games;
-
-    private String deuceMessages[] =new String[]{"Love-All","Fifteen-All","Thirty-All","Deuce","Deuce"};
+    private int player1Points;
+    private int player2Points;
     private String scoreMessages[] =new String[]{"Love","Fifteen","Thirty","Forty"};
 
     public TennisGame7(String player1Name, String player2Name) {
@@ -20,13 +14,34 @@ public class TennisGame7 implements TennisGame{
         this.player2Name = player2Name;
     }
 
-    public String getScore(){
-        return "emp";
-    }
+//    テストなどの外部からポイントを入れる関数
     public void wonPoint(String playerName){
         if (playerName == "player1")
-            player1Games += 1;
+            player1Points += 1;
         else
-            player2Games += 1;
+            player2Points += 1;
     }
+
+    public String getScore(){
+        if (this.player1Points == this.player2Points){
+//            deuceか
+            if (this.player1Points >= 3) { return "Deuce"; }
+            return scoreMessages[this.player1Points] + "-All";
+        }
+        if (this.player1Points>=4 || this.player2Points>=4) {
+            return this.advantage();
+        }
+
+        return this.scoreMessages[this.player1Points] + "-" + this.scoreMessages[this.player2Points];
+    }
+
+//    deuce以降の計算?
+    public String advantage(){
+        int pointDifference = this.player1Points - this.player2Points;
+        if (pointDifference== 1) return "Advantage player1";
+        if (pointDifference == -1) return "Advantage player2";
+        if (pointDifference>=2) return  "Win for player1";
+        return "Win for player2";
+    }
+
 }
